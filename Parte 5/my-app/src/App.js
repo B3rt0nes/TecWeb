1cd243ba1faf8f2eb6f468c0b0888a4a0e40fff0
Alberto Zuccari <albertozuccari8@gmail.com>
2023-04-28 16:37:12 +0200

    part4&5

---
+++ Parte 5/my-app/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Counter from "./counter";
+import Parent from "./parent";
+
+function App() {
+  return (
+    <div>
+      <Counter />
+      <Parent />
+    </div>
+  );
+}
+export default App;
